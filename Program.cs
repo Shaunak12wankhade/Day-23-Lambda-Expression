4aca2c65469db40ac03695961918dae63e208b08
unknown <shaunak12wankhade@ggmail.com>
2021-10-30 01:18:46 +0530

    [Shaunak] Added : Welcome Mesage

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lambda Expression");
+        }
+    }
+}
+    
+
